d443f983e9d47b2cb74f32f2b907312303513532
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-04-27 13:32:12 -0500

    Create tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 import React, { Fragment, useState } from "react";
 
 type formElement = React.FormEvent<HTMLFormElement>;
-
+interface ITask {
+  name: string;
+  done: boolean;
+}
 function App(): JSX.Element {
-  const [newTask, setNewTask] = useState<string>('');
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<ITask[]>([]);
 
-  const handleSubmit = (e:formElement ) => {
+  const handleSubmit = (e: formElement) => {
     e.preventDefault();
-    console.log(newTask);
-    
-  }
+    addTask(newTask);
+    console.log(tasks);
+    setNewTask('');
+  };
+
+  const addTask = (name: string) => {
+    const newTasks: ITask[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
+
   return (
     <Fragment>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setNewTask(e.target.value)}/> {/*Create task */}
-        <button>
-          Save
-        </button>
+        <input type="text" onChange={(e) => setNewTask(e.target.value)} value={newTask} />
+        {/*Create task */}
+        <button>Save</button>
       </form>
+      {
+        tasks.map((t: ITask, i:number) => {
+          return <h1 key={i}>{t.name}</h1>
+        })
+      }
     </Fragment>
   );
 }
